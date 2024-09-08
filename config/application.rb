1b0f2e1670a452714518d60f475a0d618f78419f
visiona <darbis@gmail.com>
2024-09-08 14:24:20 +0100

    Update keys for production environment for Railway

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative "boot"
 
 require "rails/all"
 
+
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -15,6 +17,9 @@ module MySocialApp
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
+
+    config.require_master_key = true
+
 
     # Configuration for the application, engines, and railties goes here.
     #
